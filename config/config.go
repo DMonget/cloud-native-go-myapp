f71853f99b5d44942551a4a023e4b52282184d4f
David Monget <david.monget@gmail.com>
2019-12-31 10:04:36 +0100

    Add handling of environment variables for config.

---
+++ config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"log"
+	"time"
+
+	"github.com/joeshaw/envdecode"
+)
+
+// Conf : the configuration objet for the app
+type Conf struct {
+	Server serverConf
+}
+
+type serverConf struct {
+	Port         int           `env:"SERVER_PORT,required"`
+	TimeoutRead  time.Duration `env:"SERVER_TIMEOUT_READ,required"`
+	TimeoutWrite time.Duration `env:"SERVER_TIMEOUT_WRITE,required"`
+	TimeoutIdle  time.Duration `env:"SERVER_TIMEOUT_IDLE,required"`
+}
+
+// AppConfig : returns the configuration pour our app.
+func AppConfig() *Conf {
+	var c Conf
+	if err := envdecode.StrictDecode(&c); err != nil {
+		log.Fatalf("Failed to decode: %s", err)
+	}
+
+	return &c
+}
